73f924ab89dccc3d824e93068fdefa4bc05181b3
Erick Bett <erickeno@yahoo.com>
2015-07-04 22:51:24 -0400

    Started the CMS using spina gem

---
+++ config/initializers/spina.rb
@@ -0,0 +1,10 @@
+Spina::Engine.configure do
+  config.NEGATIVE_CAPTCHA_SECRET = '4c7c4a69433f2550bfb12aa793c04ac7fe86fd092f3f7a0b5c9e84e649c8b190909d9f76788140ae04988999eb9e3ea3a25eb6b47f8879889f956490b6cf032e'
+
+  # If you want to use s3 to store uploads
+  # config.storage = :s3
+  # config.aws_region = "eu-west-1"
+  # config.aws_access_key_id = "abc123"
+  # config.aws_secret_key = "abc123"
+  # config.s3_bucket = "mybucket"
+end
